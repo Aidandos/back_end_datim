81b60528ca8bedddd775c35655682a2e2fa42429
Aidandos <timon.willi@gmail.com>
2017-12-06 02:17:05 +0100

    added another layer

--- src/main/java/hello/TrackRepository.java
+++ src/main/java/hello/TrackRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
-@Repository("userRepository")
+@Repository("trackRepository")
 public interface TrackRepository extends CrudRepository<User, Long> {
     Track findByName(String name);
     User findByID(Long id);
